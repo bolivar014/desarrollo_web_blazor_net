ffdda4478697ccc0e40dfdd65c5aa2b9caf5b617
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-09-03 08:42:55 -0500

    creando CRUD - modelo business category

--- Business/B_Category.cs
+++ Business/B_Category.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿// Importamos Capa Entity
+using Entities;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// Conexión a la base de datos
+using DataAccess;
 
 namespace Business
 {
     public class B_Category
     {
+        // Listamos los datos que exista en DB.
+        public List<CategoryEntity> CategoryList() {
+            // Generamos conexión temporal a la DB
+            using (var db = new InventaryContext()) {
+                // Retornamos el db.Context de categories
+                return db.Categories.ToList();
+            }
+        }
+
+        // Creamos Categoria
+        public void CreateCategory(CategoryEntity oCategory) {
+            // Generamos conexión temporal a la DB
+            using (var db = new InventaryContext()) {
+                // Creamos registro en Db con el OCategory -> Objeto Categoria
+                db.Categories.Add(oCategory);
+            }
+        }
+
+        // Evento para la actualización de la categoria
+        public void UpdateCategory(CategoryEntity oCategory) {
+            // Generamos conexión temporal a la DB
+            using (var db = new InventaryContext()) { 
+                // Actualizamos el objeto Categoria
+                db.Categories.Update(oCategory);
+                // Guardamos
+                db.SaveChanges();
+            }
+        }
     }
 }
