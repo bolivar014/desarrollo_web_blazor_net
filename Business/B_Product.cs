31aab8fb5d9140665cf190c66a3a1e3f582e94b7
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-09-03 08:50:24 -0500

    generando CRUD - Products

--- Business/B_Product.cs
+++ Business/B_Product.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿// Importamos Capa Entity
+using Entities;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Conexión a la base de datos
+using DataAccess;
+
 namespace Business
 {
     public class B_Product
     {
+        // Listamos los datos que exista en DB.
+        public List<ProductEntity> ProductList()
+        {
+            // Generamos conexión temporal a la DB
+            using (var db = new InventaryContext())
+            {
+                // Retornamos el db.Context de Products
+                return db.Products.ToList();
+            }
+        }
+
+        // Creamos producto
+        public void CreateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Add(oProduct);
+            }
+        }
+
+        // Actualizamos producto
+        public void UpdateProduct(ProductEntity oProduct)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Products.Update(oProduct);
+                db.SaveChanges();
+            }
+        }
     }
 }
